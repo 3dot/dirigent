a5c79cda5fe476241146e7e91903fc3066e96568
Miran Hojnik <miran@3dot.net>
2021-10-28 21:16:14 +0200

    removed old listeners

--- controllers/startup.js
+++ controllers/startup.js
@@ -1,9 +1,5 @@
 const _ = require('lodash');
 
-const state = require('../services/state');
-
-const { ws, send } = require('../services/ws').socket;
-
 module.exports = async (config) => {
     try {
         require(`./${config.service.replace('.', '/')}`).startup(config);
@@ -11,19 +7,3 @@ module.exports = async (config) => {
         console.error(err);
     }
 };
-
-state.emitter.on('set:status:container:liquidsoap', (key, [oldValue, newValue]) => {
-    //console.log(key, [oldValue, newValue]);
-    if (!oldValue) {
-        //console.log('Liquidsoap running status', newValue.state.Running);
-        return send('status', { service: "liquidsoap", running: newValue.state.Running });
-    };
-    if (oldValue.state.Status !== newValue.state.Status) {
-        //console.log('Liquidsoap running status changed', oldValue.state.Running, newValue.state.Running);
-        return send('status', { service: "liquidsoap", running: newValue.state.Running });
-    }
-});
-
-state.emitter.on('set:status:prometheus', (key, [oldValue, newValue]) => {
-    if (!_.isEqual(newValue, oldValue)) send('status', { service: "prometheus", status: newValue });
-});
