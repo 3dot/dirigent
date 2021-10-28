553f540b53f1eb27b46f9ec86382e9dd159ba4d5
Miran Hojnik <miran@3dot.net>
2021-10-28 21:15:01 +0200

    moved compose

--- services/docker.js
+++ services/docker.js
@@ -14,5 +14,27 @@ module.exports = {
             id: status[0].Id,
             state: status[0].State
         };
+    },
+    restart: async (container) => {
+        console.log(`Restarting container`, container);
+        const { stdout, stderr } = await exec(`docker restart ${container}`);
+        return stdout;
+    },
+    compose: {
+        start: async (home) => {
+            console.log('Starting docker compose');
+            const { stdout } = await exec(`cd ${home} && docker-compose up -d`);
+            return stdout;
+        },
+        restart: async (home) => {
+            console.log('Restarting docker compose');
+            const { stdout } = await exec(`cd ${home} && docker-compose restart`);
+            return stdout;
+        },
+        stop: async (home) => {
+            console.log('Stopping docker compose');
+            const { stdout } = await exec(`cd ${home} && docker-compose down`);
+            return stdout;
+        }
     }
 };
