e3287c789ab6a45544ca273ad4f6aa8967560d06
Miran Hojnik <miran@3dot.net>
2021-10-29 15:08:38 +0200

    arguments update

--- scripts/nginx-proxy.sh
+++ scripts/nginx-proxy.sh
@@ -3,7 +3,7 @@ apt install snapd;
 snap install core; snap refresh core;
 snap install --classic certbot;
 ln -s /snap/bin/certbot /usr/bin/certbot;
-certbot certonly -n -d $domain -m $email --agree-tos --standalone;
+certbot certonly -n -d $1 -m $2 --agree-tos --standalone;
 
 cd /home;
 git clone https://github.com/widecastlive/nginx-proxy.git;
