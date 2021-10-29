a70445ce72ea11138905d0c6e6e5d0b68bb7017b
Miran Hojnik <miran@3dot.net>
2021-10-29 17:09:36 +0200

    update fix

--- services/update.js
+++ services/update.js
@@ -1,7 +1,7 @@
 const exec = require('util').promisify(require('child_process').exec);
 
 module.exports = async (send) => {
-    await exec(`cd /home/dirigent && git pull && yarn install`).catch(console.error);
+    await exec(`cd /home/dirigent && git reset --hard HEAD && git clean -df && git pull && yarn install`).catch(console.error);
     await send('update:ack', { task: +new Date(), done: true });
     process.kill(process.pid, 'SIGTERM');
 };
