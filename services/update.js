bd6f2de506c3fd0bcdcc21c21e10131b4bc55bfa
Miran Hojnik <miran@3dot.net>
2021-10-29 17:11:53 +0200

    update restarting

--- services/update.js
+++ services/update.js
@@ -3,5 +3,6 @@ const exec = require('util').promisify(require('child_process').exec);
 module.exports = async (send) => {
     await exec(`cd /home/dirigent && git reset --hard HEAD && git clean -df && git pull && yarn install`).catch(console.error);
     await send('update:ack', { task: +new Date(), done: true });
-    process.kill(process.pid, 'SIGTERM');
+    await exec(`service dirigent restart`).catch(console.error);
+    //process.kill(process.pid, 'SIGTERM');
 };
