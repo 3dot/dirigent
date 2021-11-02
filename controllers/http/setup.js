23a79a310ef9420d06b1a291cfa330f6527ba206
Miran Hojnik <miran@3dot.net>
2021-11-02 11:43:04 +0100

    nginx config fix

--- controllers/http/setup.js
+++ controllers/http/setup.js
@@ -1,8 +1,7 @@
-const exec = require('util').promisify(require('child_process').exec);
 const client = require('../../services/client');
 
 const location = ({ name, ip }) => `location ~/${name}(.*)$ {
-    proxy_set_header X-Real-IP  $remote_addr;
+    proxy_set_header X-Real-IP $remote_addr;
     proxy_set_header X-Forwarded-For $remote_addr;
     proxy_set_header Host $host;
 
@@ -10,26 +9,27 @@ const location = ({ name, ip }) => `location ~/${name}(.*)$ {
     proxy_pass http://${ip}:8080$1;
 }`;
 
-module.exports = {
+const nginx = {
     insert: (home, { name, ip }) => {
-        require('fs').writeFileSync(`${home}/conf/sites/${name}.conf`, location({ name, ip }));
+        return require('fs').writeFileSync(`${home}/conf/sites/${name}.conf`, location({ name, ip }));
     },
     remove: (home, { name }) => {
-        require('fs').unlinkSync(`${home}/conf/sites/${name}.conf`);
-    },
-    fetch: {
-        all: async () => {
-            console.log('Fetch config');
-            return client.post('/config', {});
-        },
-        nginx: async (home) => {
-            // fetch config file, replace existing
-            console.log('Fetch nginx config');
-            /*const config = await client.post('/config', { config: ['liquidsoap'] });
-            if (!config.data) return;
+        return require('fs').unlinkSync(`${home}/conf/sites/${name}.conf`);
+    }
+};
 
-            await exec(`cp ${home}/radio/live.liq ${home}/radio/live.bck.liq`);
-            return require('fs').writeFileSync(`${home}/radio/live.liq`, config.data);*/
+module.exports = {
+    insert: nginx.insert,
+    remove: nginx.remove,
+    fetch: {
+        config: async (home) => {
+            // fetch config file
+            console.log('Fetch config');
+            const config = await client.post('/config', {});
+            if (!config.data) return;
+            return Promise.all(config.data.map(item => {
+                return nginx.insert(home, item);
+            }));
         }
     }
 };
